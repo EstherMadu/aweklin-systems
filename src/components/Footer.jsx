60a97b8f2a63a40a97a7201583342bda4cd73806
esthermadu <prityexsta@gmail.com>
2023-04-22 22:30:09 +0100

    issue

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,3 +3,11 @@ import React from "react";
 export const Footer = () => {
   return <div className="bg-gray-600">Footer</div>;
 };
+
+// import React from 'react'
+
+const Footer = () => {
+  return <div></div>;
+};
+
+export default Footer;
