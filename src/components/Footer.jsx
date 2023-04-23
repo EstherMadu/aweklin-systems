f247f257a5c2134a2704b89d5bafb76595229bde
esthermadu <prityexsta@gmail.com>
2023-04-23 20:29:44 +0100

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 export const Footer = () => {
-  return <div className="bg-gray-600">Footer</div>;
+  return (
+    <div className="bg-gray-600">
+      <div>
+        <ul>
+          <li>Lorem</li>
+          <li>Lorem</li>
+          <li>Lorem</li>
+          <li>Lorem</li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
