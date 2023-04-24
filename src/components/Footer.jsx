31e856cbd707d29edb7d613317d5d664a647c66e
esthermadu <prityexsta@gmail.com>
2023-04-24 19:54:24 +0100

    parallax scrolling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,17 +1,39 @@
 import React from "react";
+import codeImg from "../assets/images/code.jpg";
 
 export const Footer = () => {
   return (
-    <div className="bg-gray-600">
-      <div>
-        <ul>
-          <li>Lorem</li>
-          <li>Lorem</li>
-          <li>Lorem</li>
-          <li>Lorem</li>
-        </ul>
+    <footer className="text-white py-16">
+      <div
+        className="bg-fixed bg-center bg-cover h-96"
+        style={{
+          backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url(${codeImg})`,
+        }}
+      >
+        <div className="container mx-auto px-4 h-full flex items-center gap-8 ">
+          <h2 className="text-6xl font-bold bg-gradient-to-r from-purple-500 to-pink-500 text-transparent bg-clip-text w-72">
+            Aweklin Systems
+          </h2>
+          <div>
+            <ul>
+              <li>It Strategy Development</li>
+              <li>Process Modelling And Optimization</li>
+              <li>Systems Development</li>
+              <li>Big Data & Analytics</li>
+              <li>Data Management & Repositories</li>
+              <li>Cloud Services</li>
+              <li>Enterprise Architecture Development</li>
+            </ul>
+          </div>
+        </div>
       </div>
-    </div>
+
+      <div className="container mx-auto px-4 py-8 ">
+        <p className="text-center">
+          &copy; Aweklin Systems {new Date().getFullYear()}
+        </p>
+      </div>
+    </footer>
   );
 };
 
